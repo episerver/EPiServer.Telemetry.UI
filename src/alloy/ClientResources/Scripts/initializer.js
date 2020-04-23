1f2ab98b2f068f3adb83abefdfdb87cea809bfa8
Grzegorz Wiechec <grzegorz.wiechec@episerver.com>
2020-04-23 13:33:49 +0200

    Add sample tracker to alloy
    Code example that simplify testing tracker on Daily Site.
    
    Task: CMS-15760
    Feature: CMS-15749

---
+++ src/alloy/ClientResources/Scripts/initializer.js
@@ -0,0 +1,25 @@
+define([
+    "dojo/_base/declare",
+    "epi-cms/contentediting/command/Publish",
+    "epi/_Module",
+    "episerver-telemetry-ui/tracker"
+], function (
+    declare,
+    PublishCommand,
+    _Module,
+    tracker
+) {
+    return declare([_Module], {
+        initialize: function () {
+            var originalExecute = PublishCommand.prototype.execute;
+            PublishCommand.prototype.execute = function () {
+                tracker.track("publish-test", {
+                    "test": true
+                });
+
+                return originalExecute.apply(this, arguments);
+            };
+            PublishCommand.prototype.execute.nom = "execute";
+        }
+    });
+});
