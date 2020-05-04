9ca1eb689dd4316a934b7913b1cf9d218bf25d37
Bartosz Sekuła <Bartosz.Sekula@episerver.com>
2020-05-04 08:07:17 +0200

    v1.0.1

--- version.cs
+++ version.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Episerver AB")]
 [assembly: AssemblyProduct("Episerver.Telemetry.UI")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-developerbuild")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyInformationalVersion("1.0.1-developerbuild")]
