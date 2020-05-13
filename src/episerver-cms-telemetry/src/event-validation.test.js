21e803dd1538aab63047df7528da8ebe48ed60f3
Grzegorz Wiechec <grzegorz.wiechec@episerver.com>
2020-05-13 09:32:17 +0200

    Add event-validation module tests
    Task: CMS-15843
    Feature: CMS-15839

---
+++ src/episerver-cms-telemetry/src/event-validation.test.js
@@ -0,0 +1,51 @@
+import { validate } from './event-validation';
+
+const supportedNames = [
+    "cms_edit",                      // standard name
+    "camelCase",                     // camel case name
+    "camelCase_camelCase_camelCase"  // camel case with underscores
+];
+
+const unsupportedNames = [
+    "_cms_edit", // starts from underscore
+    "cms_edit_", // ends with underscore
+    "cms-edit",  // contains dash
+    "cms__edit", // contains double underscores
+    "cms_edit1", // contains numbers
+    "Cms_edit",  // upper camel case
+    "cms_Edit"   // upper camel case with underscores
+];
+
+describe("#validate", () => {
+
+    test.each(supportedNames)("returns `true` when event name contains '%s'", name => {
+        expect(validate(name)).toBe(true);
+    });
+
+    test.each(unsupportedNames)("returns `false` when event name contains '%s'", name => {
+        expect(validate(name)).toBe(false);
+    });
+
+    describe("when event name is valid", () => {
+        const eventName = "cms_edit";
+
+        test.each(supportedNames)("returns `true` when data name contains '%s'", name => {
+            const data = {
+                cms_info: "!@#$%^&*(_",
+                cms_content: "--------",
+            };
+            data[name] = "!@#$%^&*(_";
+            expect(validate(eventName, data)).toBe(true);
+        });
+
+        test.each(unsupportedNames)("returns `false` when data name contains '%s'", name => {
+            const data = {
+                cms_info: "!@#$%^&*(_",
+                cms_content: "--------",
+            };
+            data[name] = "!@#$%^&*(_";
+            expect(validate(eventName, data)).toBe(false);
+        });
+    });
+});
+
--- test.cmd
+++ test.cmd
@@ -11,4 +11,7 @@ ECHO Running c# tests
 CALL dotnet test src/EpiServer.Telemetry.UI.Tests/EpiServer.Telemetry.UI.Tests.csproj --no-build --verbosity normal
 IF %errorlevel% NEQ 0 EXIT /B %errorlevel%
 
+ECHO Running JS tests
+CALL yarn --cwd src/episerver-cms-telemetry test
+
 EXIT /B %ERRORLEVEL%
