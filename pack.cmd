85ad25a62240d763d1dda5a61e4467446f4f707b
Grzegorz Wiechec <grzegorz.wiechec@episerver.com>
2020-05-22 12:06:19 +0200

    Create a package when call pack.cmd
    Feature: CMS-15839
    Task: CMS-15897

--- pack.cmd
+++ pack.cmd
@@ -1,5 +1,20 @@
+@ECHO OFF
+SETLOCAL
+SET PATH=.\.ci\tools\;%PATH%
+
 powershell ./pack.ps1
 powershell ./pack-core.ps1
 powershell ./pack-alloy.ps1
 REM Tell TeamCity to publish the artifacts even though the entire build isn't done
 ECHO ##teamcity[publishArtifacts '*.nupkg']
+
+REM Create the packaged node module for distribution.
+CALL yarn --cwd src/episerver-cms-telemetry pack
+IF %errorlevel% NEQ 0 EXIT /B %errorlevel%
+
+ECHO Contents of the tarball:
+FOR /f %%x IN ('dir /b src\episerver-cms-telemetry\episerver-cms-telemetry-v*.tgz') DO SET tarball=%%x
+CALL tar -ztvf src/episerver-cms-telemetry/%tarball%
+IF %errorlevel% NEQ 0 EXIT /B %errorlevel%
+
+EXIT /B %ERRORLEVEL%
