e230ab6687af886413ec935af23a0026766ca681
Grzegorz Wiechec <grzegorz.wiechec@episerver.com>
2020-08-12 11:40:05 +0200

    Move functions out parent function scope
    Feature: CMS-16113
    Related: CMS-16304

--- src/AlloyMvcTemplates/modules/_protected/episerver-telemetry-ui/1.0.0/Scripts/track-projects.js
+++ src/AlloyMvcTemplates/modules/_protected/episerver-telemetry-ui/1.0.0/Scripts/track-projects.js
@@ -7,21 +7,22 @@ define([
     AddProject,
     tracker
 ) {
+    function patchAddProjectCommand() {
+        var originalOnDialogExecute = AddProject.prototype.onDialogExecute;
+        AddProject.prototype.onDialogExecute = function () {
+            originalOnDialogExecute.apply(this, arguments);
+            tracker.trackEvent("project_create");
+        };
+        AddProject.prototype.onDialogExecute.nom = "onDialogExecute";
+    }
+
+    function onShowProjectOverview(args) {
+        if (args.uri && args.uri.indexOf("epi.cms.project:///") !== -1) {
+            tracker.trackEvent("project_overview");
+        }
+    }
+
     return function () {
-        function patchAddProjectCommand() {
-            var originalOnDialogExecute = AddProject.prototype.onDialogExecute;
-            AddProject.prototype.onDialogExecute = function () {
-                originalOnDialogExecute.apply(this, arguments);
-                tracker.trackEvent("project_create");
-            }
-        }
-
-        function onShowProjectOverview(args) {
-            if (args.uri && args.uri.indexOf("epi.cms.project:///") !== -1) {
-                tracker.trackEvent("project_overview");
-            }
-        }
-
         topic.subscribe("/epi/shell/context/request", onShowProjectOverview);
 
         patchAddProjectCommand();
