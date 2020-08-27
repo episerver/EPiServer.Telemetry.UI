37c457fdb8e5576169ca29d5b6912b53ab90b962
Grzegorz Wiechec <grzegorz.wiechec@episerver.com>
2020-08-27 09:48:49 +0200

    v1.0.9

--- version.cs
+++ version.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Episerver AB")]
 [assembly: AssemblyProduct("Episerver Telemetry UI")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.8")]
-[assembly: AssemblyInformationalVersion("1.0.8-developerbuild")]
+[assembly: AssemblyVersion("1.0.9")]
+[assembly: AssemblyInformationalVersion("1.0.9-developerbuild")]
