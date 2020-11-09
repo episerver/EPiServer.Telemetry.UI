ffa6ab7810ae0e681d68a8aa4219c162566992da
Alex Wang <alex.wang@episerver.com>
2020-11-09 16:38:09 +0100

    Fix typo in entryPoint
    It should be contextMenu.
    
    Fixes: CMS-17388

--- src/AlloyMvcTemplates/modules/_protected/episerver-telemetry-ui/1.0.0/Scripts/track-creation.js
+++ src/AlloyMvcTemplates/modules/_protected/episerver-telemetry-ui/1.0.0/Scripts/track-creation.js
@@ -63,7 +63,7 @@ define([
         // _execute
         var originalExecute = NewContentCommand.prototype._execute;
         NewContentCommand.prototype._execute = function () {
-            entry.entryPoint = this.category === "context" ? "contentMenu" : "gadgetIcon";
+            entry.entryPoint = this.category === "context" ? "contextMenu" : "gadgetIcon";
             trackOpenCreate.call(this);
             originalExecute.apply(this, arguments);
         };
